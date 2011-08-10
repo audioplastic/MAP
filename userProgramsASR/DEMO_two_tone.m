6e0fd8261e2a0b278a5c850e40e43c1eba1b814d
Nick Clark <nrclark@essex.ac.uk>
2011-08-10 15:28:33 +0100

    trying to reproduce experiment 4 results t in nz

--- userProgramsASR/DEMO_two_tone.m
+++ userProgramsASR/DEMO_two_tone.m
@@ -8,13 +8,24 @@ x.ARthreshold_dB = 200;
 
 
 dt = 1/x.sr;
-tAxis = dt:dt:0.1;
+tAxis = dt:dt:0.2;
 f = [x.channelBFs(3)*2^0.2 x.channelBFs(4)*2^-0.2];
 dBlev = 60;
 s1 = sin(2*pi*f(1)*tAxis);
-s1 = s1/max(s1);%/sqrt(mean(s1.^2));
+
 
 s2 = sin(2*pi*f(2)*tAxis);
-s2 = s2/max(s2);%/sqrt(mean(s2.^2));
 
-x = x.processStim
+
+figure; plot(s1+s2)
+
+
+s1 = s1/sqrt(mean(s1.^2));
+s2 = s2/sqrt(mean(s2.^2));
+
+s1 = s1*20e-6*10^(dBlev/20);
+s2 = s2*20e-6*10^(dBlev/20);
+
+x.stimulusUSER = s1+s2;
+x = x.processStim;
+figure; plot(x)
