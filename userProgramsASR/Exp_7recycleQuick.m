595e4d82143e97e36bef6e999e4b8ac095ba1f52
Nick Clark <nrclark@essex.ac.uk>
2011-08-25 14:34:19 +0100

    trying auto eff in new framework shallow eff

--- userProgramsASR/Exp_7recycleQuick.m
+++ userProgramsASR/Exp_7recycleQuick.m
@@ -67,7 +67,7 @@ tmpIdx=0;
 for nn = 0*recConditions+1:1*recConditions    
     tmpIdx=tmpIdx+1;
     xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['NEW_AUTOfeatR_cond' num2str(nn)]);
+    recFolder = fullfile(expFolder,['NEW_SHALfeatR_cond' num2str(nn)]);
     xR{nn}.opFolder = recFolder;    
     
     %These are the interesting differences between training and testing
