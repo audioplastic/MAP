54a8689c36b8d298f0c0e924ac480ed56663820b
Nick Clark <nrclark@essex.ac.uk>
2011-08-25 14:45:06 +0100

    trying auto eff in new framework steeper eff

--- userProgramsASR/RateLevel_view.m
+++ userProgramsASR/RateLevel_view.m
@@ -17,7 +17,7 @@ freq = 1000;
 
 nn=0;
 % for levelSPL = 40:10:70;
-for levelSPL = [55 60]
+for levelSPL = [55 60 65]
 % levelSPL = 50;
 nn = nn+1;
 levelRec(nn) = levelSPL;
@@ -46,9 +46,9 @@ ipSig = ipSig./sqrt(mean(ipSig.^2));
 ipSig = ipSig * 20e-6 * 10 ^ (levelSPL/20);
 
 paramChanges = {};
-paramChanges{numel(paramChanges)+1} = 'DRNLParams.rateToAttenuationFactorProb =  0.010;';%GOOD = 0.012  %DEFAULT = 0.005;  % strength of MOC
+paramChanges{numel(paramChanges)+1} = 'DRNLParams.rateToAttenuationFactorProb =  0.012;';%GOOD = 0.012  %DEFAULT = 0.005;  % strength of MOC
 % paramChanges{numel(paramChanges)+1} = 'DRNLParams.rateToAttenuationFactor =  0.005;';
-paramChanges{numel(paramChanges)+1} = 'DRNLParams.MOCrateThresholdProb = 57;';%GOOD=140 %DEFAULT = 70;
+paramChanges{numel(paramChanges)+1} = 'DRNLParams.MOCrateThresholdProb = 65;';%GOOD=140 %DEFAULT = 70;
 % paramChanges{numel(paramChanges)+1} = 'DRNLParams.MOCrateThreshold = 50;'
 paramChanges{numel(paramChanges)+1} = 'DRNLParams.MOCtau = 0.35;'; %DEFAULT = 0.1;
 % paramChanges{numel(paramChanges)+1} = 'DRNLParams.a = 10000;';
