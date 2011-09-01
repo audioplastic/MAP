7649ea982bebcea2da470b475ea97fcd51d81d90
Nick Clark <nrclark@essex.ac.uk>
2011-09-01 21:25:07 +0100

    testing different TCs in babble condition

--- userProgramsASR/Exp_7recycleQuick.m
+++ userProgramsASR/Exp_7recycleQuick.m
@@ -67,7 +67,7 @@ tmpIdx=0;
 for nn = 0*recConditions+1:1*recConditions    
     tmpIdx=tmpIdx+1;
     xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['LOGbfeatR_cond' num2str(nn)]);
+    recFolder = fullfile(expFolder,['TCfeatR_cond' num2str(nn)]);
     xR{nn}.opFolder = recFolder;    
     
     %These are the interesting differences between training and testing
@@ -90,7 +90,7 @@ tmpIdx=0;
 for nn = 1*recConditions+1:2*recConditions    
     tmpIdx=tmpIdx+1;
     xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['LOGbfeatR_cond' num2str(nn)]);
+    recFolder = fullfile(expFolder,['TCfeatR_cond' num2str(nn)]);
     xR{nn}.opFolder = recFolder;    
     
     %These are the interesting differences between training and testing
@@ -113,7 +113,7 @@ tmpIdx=0;
 for nn = 2*recConditions+1:3*recConditions    
     tmpIdx=tmpIdx+1;
     xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['LOGbfeatR_cond' num2str(nn)]);
+    recFolder = fullfile(expFolder,['TCfeatR_cond' num2str(nn)]);
     xR{nn}.opFolder = recFolder;    
     
     %These are the interesting differences between training and testing
@@ -135,7 +135,7 @@ tmpIdx=0;
 for nn = 3*recConditions+1:4*recConditions    
     tmpIdx=tmpIdx+1;
     xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['LOGbfeatR_cond' num2str(nn)]);
+    recFolder = fullfile(expFolder,['TCfeatR_cond' num2str(nn)]);
     xR{nn}.opFolder = recFolder;    
     
     %These are the interesting differences between training and testing
@@ -157,7 +157,7 @@ tmpIdx=0;
 for nn = 4*recConditions+1:5*recConditions    
     tmpIdx=tmpIdx+1;
     xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['LOGbfeatR_cond' num2str(nn)]);
+    recFolder = fullfile(expFolder,['TCfeatR_cond' num2str(nn)]);
     xR{nn}.opFolder = recFolder;    
     
     %These are the interesting differences between training and testing
@@ -180,7 +180,7 @@ tmpIdx=0;
 for nn = 5*recConditions+1:6*recConditions    
     tmpIdx=tmpIdx+1;
     xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['LOGbfeatR_cond' num2str(nn)]);
+    recFolder = fullfile(expFolder,['TCfeatR_cond' num2str(nn)]);
     xR{nn}.opFolder = recFolder;    
     
     %These are the interesting differences between training and testing
