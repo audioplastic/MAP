8807d0112dc08d76dbb636106bec3406a6a01f1b
Nick Clark <nrclark@essex.ac.uk>
2011-09-02 12:33:34 +0100

    1000 TC monster w 8sec lead in

--- userProgramsASR/Exp_7recycleQuick.m
+++ userProgramsASR/Exp_7recycleQuick.m
@@ -67,13 +67,13 @@ tmpIdx=0;
 for nn = 0*recConditions+1:1*recConditions    
     tmpIdx=tmpIdx+1;
     xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['TC4000featR_cond' num2str(nn)]);
+    recFolder = fullfile(expFolder,['TC1000_l8_featR_cond' num2str(nn)]);
     xR{nn}.opFolder = recFolder;    
     
     %These are the interesting differences between training and testing
     xR{nn}.numWavs = testWavs; %MAX = 358
     xR{nn}.noiseLevToUse = nzLevel(tmpIdx);
-    xR{nn}.MAPparamChanges= {'DRNLParams.rateToAttenuationFactorProb = 7;','DRNLParams.MOCrateThresholdProb = 85;', 'DRNLParams.MOCtau = 4;'};
+    xR{nn}.MAPparamChanges= {'DRNLParams.rateToAttenuationFactorProb = 7;','DRNLParams.MOCrateThresholdProb = 85;', 'DRNLParams.MOCtau = 1;'};
     
     
     %Now just to wrap it up ready for processing
