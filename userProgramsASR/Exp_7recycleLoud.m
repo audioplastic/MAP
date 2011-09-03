a3ad4b155292981bcdb72c0ed48ad0dd25eb3e64
Nick Clark <nrclark@essex.ac.uk>
2011-09-03 12:59:29 +0100

    quick AR higher threshold

--- userProgramsASR/Exp_7recycleLoud.m
+++ userProgramsASR/Exp_7recycleLoud.m
@@ -26,10 +26,10 @@ learnFolder = fullfile(expFolder,'featL');
 xL = jobject('L', learnFolder);
 
 xL.participant = 'NormalDIFF';%'NormalDIFF';
-xL.MAPparamChanges= {' DRNLParams.rateToAttenuationFactorProb = 0;', 'OMEParams.rateToAttenuationFactorProb=0;' , 'OMEParams.ARrateThreshold = 25;', 'OMEParams.ARtau=0.05;'};
+xL.MAPparamChanges= {' DRNLParams.rateToAttenuationFactorProb = 0;', 'OMEParams.rateToAttenuationFactorProb=0;' , 'OMEParams.ARrateThreshold = 40;', 'OMEParams.ARtau=0.1;'};
 
 xL.noiseLevToUse   =  -200;
-xL.speechLevToUse  =  75;
+xL.speechLevToUse  =  80;
 
 xL.MAPopHSR = 1;
 xL.MAPopMSR = 0;
@@ -41,8 +41,8 @@ xL.removeEnergyStatic = 0;
 
 %%%%% Group of params that will influence simulation run time %%%%%%%
 xL.numWavs = 8440; %MAx=8440
-testWavs = 358; %MAX = 358
-nzLevel = [-200 40:5:50]+15;
+testWavs = 150; %MAX = 358
+nzLevel = [-200 40:5:50]+20;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 xL.noisePreDur = 4;
@@ -69,13 +69,13 @@ tmpIdx=0;
 for nn = 0*recConditions+1:1*recConditions    
     tmpIdx=tmpIdx+1;
     xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['ARQcond' num2str(nn)]);
+    recFolder = fullfile(expFolder,['ARQhcond' num2str(nn)]);
     xR{nn}.opFolder = recFolder;    
     
     %These are the interesting differences between training and testing
     xR{nn}.numWavs = testWavs; %MAX = 358
     xR{nn}.noiseLevToUse = nzLevel(tmpIdx);
-    xR{nn}.MAPparamChanges= {'DRNLParams.rateToAttenuationFactorProb = 7;','DRNLParams.MOCrateThresholdProb = 85;', 'DRNLParams.MOCtau = 2;', 'OMEParams.rateToAttenuationFactorProb = 6;'};
+    xR{nn}.MAPparamChanges= {'DRNLParams.rateToAttenuationFactorProb = 7;','DRNLParams.MOCrateThresholdProb = 85;', 'DRNLParams.MOCtau = 2;', 'OMEParams.rateToAttenuationFactorProb = 8;'};
     
     
     %Now just to wrap it up ready for processing
@@ -87,27 +87,7 @@ for nn = 0*recConditions+1:1*recConditions
     end
 end
 
-tmpIdx=0;
-for nn = 1*recConditions+1:2*recConditions    
-    tmpIdx=tmpIdx+1;
-    xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['ARQcond' num2str(nn)]);
-    xR{nn}.opFolder = recFolder;    
-    
-    %These are the interesting differences between training and testing
-    xR{nn}.numWavs = testWavs; %MAX = 358
-    xR{nn}.noiseLevToUse = nzLevel(tmpIdx);
-    xR{nn}.MAPparamChanges= {'DRNLParams.rateToAttenuationFactorProb = 7;','DRNLParams.MOCrateThresholdProb = 85;', 'DRNLParams.MOCtau = 2;', 'OMEParams.rateToAttenuationFactorProb = 7;'};
-    
-    
-    %Now just to wrap it up ready for processing
-    if isMasterNode && ~isdir(xR{nn}.opFolder)
-        mkdir(xR{nn}.opFolder);
-        xR{nn} = xR{nn}.assignWavPaths('R');
-        xR{nn} = xR{nn}.assignFiles;
-        xR{nn}.storeSelf;
-    end
-end
+
 
 
 
@@ -123,7 +103,7 @@ end
 % worker(xL.opFolder);
 
 if ~isMasterNode %dont bother wasting master node effort on generating testing features (for now)
-    for nn = 0*recConditions+1:2*recConditions
+    for nn = 0*recConditions+1:1*recConditions
         worker(xR{nn}.opFolder);
     end
 end
@@ -147,7 +127,7 @@ if isMasterNode
     
     % ALLOW MASTER NODE TO MUCK IN WITH GENERATING TESTING FEATURES ONCE
     % HMM HAS BEEN TRAINED
-    for nn = 0*recConditions+1:2*recConditions
+    for nn = 0*recConditions+1:1*recConditions
         worker(xR{nn}.opFolder);
     end    
     
@@ -162,13 +142,13 @@ if isMasterNode
         xR{end}.unlockJobList;
     end
       
-    for nn = 0*recConditions+1:2*recConditions
+    for nn = 0*recConditions+1:1*recConditions
         y.createSCP(xR{nn}.opFolder);
         y.test(xR{nn}.opFolder);
     end
     
     %Show all of the scores in the command window at the end
-    for nn = 0*recConditions+1:2*recConditions
+    for nn = 0*recConditions+1:1*recConditions
         y.score(xR{nn}.opFolder);
     end
 end
