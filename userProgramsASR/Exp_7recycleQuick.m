392c22d14c696f3ff2c133a59708981a5467f3fd
Nick Clark <nrclark@essex.ac.uk>
2011-09-04 17:14:46 +0100

    going back in time to Thursday night in new branch

--- userProgramsASR/Exp_7recycleQuick.m
+++ userProgramsASR/Exp_7recycleQuick.m
@@ -67,7 +67,7 @@ tmpIdx=0;
 for nn = 0*recConditions+1:1*recConditions    
     tmpIdx=tmpIdx+1;
     xR{nn} = xL; %simply copy the "Learn" object and change it a bit below
-    recFolder = fullfile(expFolder,['TC2000_pre4_r2a700_featR_cond' num2str(nn)]);
+    recFolder = fullfile(expFolder,['TC2000_pre4_r2a700_featRB_cond' num2str(nn)]);
     xR{nn}.opFolder = recFolder;    
     
     %These are the interesting differences between training and testing
