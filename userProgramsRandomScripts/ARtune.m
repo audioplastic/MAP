a5ad9c4194998b05a0412d333fc0a77413c9429d
Nick Clark <nrclark@essex.ac.uk>
2011-11-28 16:42:14 +0000

    param tweak

--- userProgramsRandomScripts/ARtune.m
+++ userProgramsRandomScripts/ARtune.m
@@ -13,12 +13,15 @@ close all; clear all; clc
 %             zeros(1, ceil(silDuration*sr))];
 
 %%
-speechLev = 100;
+speechLev = 90;
 noiseLev = speechLev-10;
 [stimulus,sr] = wavread(fullfile('..','userProgramsASR','demo_wavs','microAURORA','MHS_2841A'));
 % stimulus = randn(44100,1); sr = 44.1e3;
 stimulus = 20e-6*10^(speechLev/20)  *  stimulus./(sqrt(mean(stimulus.^2)));
-stimulus = [zeros(44100,1); stimulus];
+stimulus = [zeros(6*sr,1); stimulus];
+noise = randn(size(stimulus));
+noise = 20e-6*10^(noiseLev/20)  *  noise./(sqrt(mean(noise.^2)));
+stimulus=stimulus+noise;
 dtL = 1/sr;
 
 %%
@@ -31,7 +34,8 @@ addpath(...
 global ANprobRateOutput  ANdt savedBFlist MOCattenuation DRNLParams ARattenuation
 
 paramChanges= { 'OMEParams.rateToAttenuationFactorProb=12;',...
-                'OMEParams.ARrateThreshold=45;',... %Threshold of 40 makes AR kick off around 65 dB for bb noise
+                'OMEParams.ARrateThreshold=30;',... %Threshold of 40 makes AR kick off around 65 dB for bb noise
+                'OMEParams.ARtau=0.1;',...
                 'DRNLParams.MOCtauR=2;',...
                 'DRNLParams.MOCtauF=2;',...                
                 'DRNLParams.rateToAttenuationFactorProb=9;',...
@@ -47,14 +51,19 @@ tAxisAN = dtL:dtL:dtL*numel(HSRprob);
 
 YTickIdx = 1:floor(numel(savedBFlist)/6):numel(savedBFlist);
 YTickIdxRev = numel(savedBFlist)+1-YTickIdx;
-subplot(3,1,1); imagesc(HSRprob); colorbar('peer', gca)
+subplot(4,1,1); imagesc(HSRprob); colorbar('peer', gca)
 set(gca, 'YTick', YTickIdx);
 set(gca, 'YTickLabel', num2str(    savedBFlist(YTickIdxRev)'     ));
 ylabel('cf in Hz')
 
-subplot(3,1,2); imagesc(20*log10(MOCattenuation)); colorbar('peer', gca)
+subplot(4,1,2); imagesc(LSRprob); colorbar('peer', gca)
 set(gca, 'YTick', YTickIdx);
 set(gca, 'YTickLabel', num2str(    savedBFlist(YTickIdxRev)'     ));
 ylabel('cf in Hz')
 
-subplot(3,1,3); plot(20*log10(ARattenuation));colorbar
+subplot(4,1,3); imagesc(20*log10(MOCattenuation)); colorbar('peer', gca)
+set(gca, 'YTick', YTickIdx);
+set(gca, 'YTickLabel', num2str(    savedBFlist(YTickIdxRev)'     ));
+ylabel('cf in Hz')
+
+subplot(4,1,4); plot(20*log10(ARattenuation));colorbar
