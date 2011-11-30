8bd864fe2301702784a69c58abf3b5d3f61db78b
Nick Clark <nrclark@essex.ac.uk>
2011-11-30 11:53:03 +0000

    bugfix

--- userProgramsASR/Exp_14_LevIndR.m
+++ userProgramsASR/Exp_14_LevIndR.m
@@ -96,7 +96,7 @@ for nn = 0*recConditions+1:1*recConditions
     xR{nn}.noiseLevToUse = nzLevel(tmpIdx);
     xR{nn}.speechDist = 'None';
     xR{nn}.noiseDist = 'None';
-    xR{nn}.MAPparamChanges= { 'DRNLParams.a=400' };
+    xR{nn}.MAPparamChanges= { 'DRNLParams.a=400;' };
     
     xR{nn}.useAid=0;
 %     xR{nn}.aidInstance = newAid;
@@ -127,7 +127,7 @@ for nn = 1*recConditions+1:2*recConditions
     xR{nn}.noiseLevToUse = nzLevel(tmpIdx);
     xR{nn}.speechDist = 'None';
     xR{nn}.noiseDist = 'None';
-    xR{nn}.MAPparamChanges= { 'DRNLParams.a=400' };
+    xR{nn}.MAPparamChanges= { 'DRNLParams.a=400;' };
     
     xR{nn}.useAid=1;
     xR{nn}.aidInstance = newAid;
@@ -156,7 +156,7 @@ for nn = 2*recConditions+1:3*recConditions
     xR{nn}.noiseLevToUse = nzLevel(tmpIdx);
     xR{nn}.speechDist = 'None';
     xR{nn}.noiseDist = 'None';
-    xR{nn}.MAPparamChanges= { 'DRNLParams.a=400' };
+    xR{nn}.MAPparamChanges= { 'DRNLParams.a=400;' };
     
     xR{nn}.useAid=1;
     xR{nn}.aidInstance = newAid;
@@ -185,7 +185,7 @@ for nn = 3*recConditions+1:4*recConditions
     xR{nn}.noiseLevToUse = nzLevel(tmpIdx);
     xR{nn}.speechDist = 'None';
     xR{nn}.noiseDist = 'None';
-    xR{nn}.MAPparamChanges= { 'DRNLParams.a=400' };
+    xR{nn}.MAPparamChanges= { 'DRNLParams.a=400;' };
     
     xR{nn}.useAid=1;
     xR{nn}.aidInstance = newAid;
